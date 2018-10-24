4e54cc7c789b3753fbd19e5ec96638fdf3912fe8
Lui Kohl <lui.kohl@gmail.com>
2018-10-24 17:52:20 +0200

    slider component

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,18 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
-import {getMetricMetaInfo} from '../utils/helpers';
+import {View, TouchableOpacity, Text} from 'react-native';
+import {getMetricMetaInfo, timeToString} from '../utils/helpers';
+import SuperFitSlider from './SuperFitSlider';
+import SuperFitSteppers from './SuperFitSteppers';
+import DateHeader from './DateHeader';
+
+function SubmitBtn({onPress}) {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Text>SUBMIT</Text>
+
+    </TouchableOpacity>
+  );
+}
 
 export default class AddEntry extends Component {
   state = {
@@ -10,10 +22,82 @@ export default class AddEntry extends Component {
     sleep: 0,
     eat: 0,
   };
+
+  increment = metric => {
+    const {max, Step} = getMetricMetaInfo (metric);
+    this.setState (state => {
+      const count = state[metric] + step;
+
+      return {
+        ...state,
+        [metric]: count > max ? max : count,
+      };
+    });
+  };
+
+  decrement = metric => {
+    this.setState (state => {
+      const count = state[metric] - getMetricMetaInfo (metric).step;
+
+      return {
+        ...state,
+        [metric]: count < 0 ? 0 : count,
+      };
+    });
+  };
+  slide = (metric, value) => {
+    this.setState (() => ({
+      [metric]: value,
+    }));
+  };
+  submit = () => {
+    const key = timeToString ();
+    const entry = this.state;
+
+    // Update Redux
+
+    this.setState (() => ({
+      run: 0,
+      bike: 0,
+      swim: 0,
+      sleep: 0,
+      eat: 0,
+    }));
+
+    // Navigate to home
+
+    // Save to DB
+
+    // Clear local notifications
+  };
   render () {
+    const metaInfo = getMetricMetaInfo ();
     return (
       <View>
-        {getMetricMetaInfo ('bike').getIcon ()}
+        <DateHeader date={new Date ().toLocaleDateString ()} />
+        {Object.keys (metaInfo).map (key => {
+          const {getIcon, type, ...rest} = metaInfo[key];
+          const value = this.state[key];
+
+          return (
+            <View key={key}>
+              {getIcon ()}
+              {type === 'slider'
+                ? <SuperFitSlider
+                    value={value}
+                    onChange={value => this.slide (key, value)}
+                    {...rest}
+                  />
+                : <SuperFitSteppers
+                    value={value}
+                    onIncrement={() => this.increment (key)}
+                    onDecrement={() => this.decrement (key)}
+                    {...rest}
+                  />}
+            </View>
+          );
+        })}
+        <SubmitBtn onPress={this.submit} />
       </View>
     );
   }
